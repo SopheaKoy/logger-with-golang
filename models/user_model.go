6078bf32a4d9411c7ee7ecce92f906efd651c9be
sophea <koysophea09@gmail.com>
2025-05-12 19:03:26 +0700

    init model

--- models/user_model.go
+++ models/user_model.go
@@ -15,6 +15,7 @@ type UserModel struct {
 	Password	string    `json:"-" gorm:"not null"`
 	CreatedAt   time.Time `json:"created_at" gorm:"autoCreateTime"`
 	UpdatedAt   time.Time `json:"updated_at" gorm:"autoUpdateTime"`
+	DeletedAt	time.Time `json:"deleted_at" gorm:"autoDeleteTime"`
 }
 
 /*
